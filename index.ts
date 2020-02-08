46dd2812339022d27fc429e6afb6150606e65044
Anwesh43 <anweshthecool0@gmail.com>
2020-02-08 20:13:50 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -102,3 +102,38 @@ class ScaleUtil {
         return Math.sin(sf * Math.PI / 2)
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawHorizontalLinePuncher(context : CanvasRenderingContext2D, i : number, scale : number, w : number, size : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sfi : number = ScaleUtil.divideScale(sf, i, lines)
+        const sj : number = 1 - 2 * j
+        const sx : number = w * i
+        const x : number = (w / 2) * sfi * sj
+        context.save()
+        context.translate(sx, 0)
+        DrawingUtil.drawLine(context, 0, 0, x, 0)
+        DrawingUtil.drawLine(context, x, 0, x, -size * sj)
+        context.restore()
+    }
+
+    static drawHLPNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const gap : number = h / (nodes + 1)
+        const size : number = gap / sizeFactor
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = foreColor
+        context.save()
+        context.translate(0, gap * (i + 1))
+        DrawingUtil.drawHorizontalLinePuncher(context, i, scale, w, size)
+        context.restore()
+    }
+}
